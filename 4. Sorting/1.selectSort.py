4b2e69c3eec682999ff187767121b0be502383c4
redgirl104 <>
2021-04-20 16:20:11 +0000

    update 4.1

--- 4. Sorting/1.selectSort.py
+++ 4. Sorting/1.selectSort.py
@@ -1,1 +1,17 @@
-print('filename:' , __file__)
+
+"""
+선택정렬 : 처리되지 않은 데이터 중에서 가장 작은 원소를 가장 왼쪽의 데이터와 교환
+"""
+
+input_list = list(map(int, input().split()))
+
+for i in range(len(input_list)):
+  pointer = i
+  for j in range(i+1, len(input_list)):
+    if input_list[j] < input_list[pointer]:
+      pointer = j
+  input_list[i], input_list[pointer] = input_list[pointer], input_list[i]
+
+
+print(input_list)
+
